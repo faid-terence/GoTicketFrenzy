81da9e5dffa71a9ed85f5003b4d3a4c65eff53bc
Terence Faid JABO <faidterence@outlook.com>
2024-06-30 10:27:11 +0200

    Get user Inputs

--- main.go
+++ main.go
@@ -7,10 +7,21 @@ func main() {
 	const ConferenceTickets = 20
 	var remainingTickets = 20
 
-	fmt.Println("Welcome to the", conferenceName, "booking application")
-	fmt.Println("We have a total of ", ConferenceTickets, "and only", remainingTickets, "tickets remaining")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v and only %v tickets remaining\n", ConferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets now!")
 
+
+	var UserName string
+	var UserTickets int
+
+	// Get user details
+	fmt.Println("Please enter your name: ")
+	fmt.Scan(&UserName)
+	fmt.Println("How many tickets would you like to book?")
+	fmt.Scan(&UserTickets)
+
+	fmt.Printf("Thank you %v for booking %v tickets\n", UserName, UserTickets)
 
 }
 
