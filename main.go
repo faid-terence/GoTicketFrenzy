51ab61ed4d0128fa2e0d8e32d74ee965b902753b
Terence Faid JABO <faidterence@outlook.com>
2024-06-30 20:48:44 +0200

    Add logical program execution

--- main.go
+++ main.go
@@ -31,21 +31,34 @@ func main() {
 	fmt.Scan(&UserEmail)
 	fmt.Println("How many tickets would you like to book?")
 	fmt.Scan(&UserTickets)
+
+	if UserTickets <= remainingTickets {
+		fmt.Printf("Sorry, we only have %v tickets remaining\n", remainingTickets)
+		remainingTickets = remainingTickets - UserTickets
+		bookings = append(bookings, firstName + " " + lastName)
 	
-	remainingTickets = remainingTickets - UserTickets
-	bookings = append(bookings, firstName + " " + lastName)
+	
+		fmt.Printf("Thank you %v for booking %v tickets , you will receive a notification on %v , please check your inbox!!!\n", lastName, UserTickets, UserEmail)
+		fmt.Printf("We have %v tickets remaining\n", remainingTickets)
+	
+		firstNames:= []string {}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+	
+		fmt.Printf("First names of people who have booked: %v\n", firstNames)
 
-
-	fmt.Printf("Thank you %v for booking %v tickets , you will receive a notification on %v , please check your inbox!!!\n", lastName, UserTickets, UserEmail)
-	fmt.Printf("We have %v tickets remaining\n", remainingTickets)
-
-	firstNames:= []string {}
-	for _, booking := range bookings {
-		var names = strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
+		if remainingTickets == 0 {
+			fmt.Println("Sorry, we are sold out!")
+			break
+		}
+	}else {
+		fmt.Printf("Sorry, we only have %v tickets remaining\n", remainingTickets)
 	}
+	
 
-	fmt.Printf("First names of people who have booked: %v\n", firstNames)
+
 
 	}
 
